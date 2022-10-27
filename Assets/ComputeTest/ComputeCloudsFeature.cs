f7b07dd02a4092d951006f00a77d4925ebd2bfd3
Alex Malyutin <alex.malyutin.dev@gmail.com>
2022-10-27 19:10:56 +0400

    Add projection matrix.

--- Assets/ComputeTest/ComputeCloudsFeature.cs
+++ Assets/ComputeTest/ComputeCloudsFeature.cs
@@ -15,12 +15,14 @@ public class ComputeCloudsFeature : ScriptableRendererFeature
 
         private RenderTargetHandle _CloudsLightingTextureRW;
         private readonly int _cloudsLightingTextureRW_ID = Shader.PropertyToID("_CloudsLightingTextureRW");
-        
-        private Texture _volume;
 
-        public CustomRenderPass(ComputeShader cloudsShader, Texture volume)
+        private Texture _volume;
+        private readonly Material _blitMat;
+
+        public CustomRenderPass(ComputeShader cloudsShader, Texture volume, Material blitMat)
         {
             _volume = volume;
+            _blitMat = blitMat;
             _cloudsShader = cloudsShader;
             _renderCloudsKernel = _cloudsShader.FindKernel("RenderClouds");
             _CloudsLightingTextureRW = new RenderTargetHandle("_CloudsLightingTextureRW");
@@ -29,26 +31,29 @@ public class ComputeCloudsFeature : ScriptableRendererFeature
 
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
-            cmd.GetTemporaryRT(_CloudsLightingTextureRW.id, new RenderTextureDescriptor()
-            {
-                colorFormat = RenderTextureFormat.Default,
-                width = (int)_TraceScreenSize.x,
-                height = (int)_TraceScreenSize.y,
-                autoGenerateMips = false,
-                bindMS = false,
-                depthBufferBits = 32,
-                enableRandomWrite = true,
-                dimension = TextureDimension.Tex2D,
-                msaaSamples = 1
-            });
+            cmd.GetTemporaryRT(
+                _CloudsLightingTextureRW.id,
+                new RenderTextureDescriptor()
+                {
+                    colorFormat = RenderTextureFormat.Default,
+                    width = (int) _TraceScreenSize.x,
+                    height = (int) _TraceScreenSize.y,
+                    autoGenerateMips = false,
+                    bindMS = false,
+                    depthBufferBits = 32,
+                    enableRandomWrite = true,
+                    dimension = TextureDimension.Tex2D,
+                    msaaSamples = 1
+                }
+            );
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             // Compute the number of tiles to evaluate
-            int traceTX = ((int)_TraceScreenSize.x + (8 - 1)) / 8;
-            int traceTY = ((int)_TraceScreenSize.y + (8 - 1)) / 8;
-            
+            int traceTX = ((int) _TraceScreenSize.x + (8 - 1)) / 8;
+            int traceTY = ((int) _TraceScreenSize.y + (8 - 1)) / 8;
+
             var cmd = CommandBufferPool.Get();
             using (new ProfilingScope(cmd, profilingSampler))
             {
@@ -56,19 +61,31 @@ public class ComputeCloudsFeature : ScriptableRendererFeature
                 cmd.Clear();
 
                 _cloudsShader.SetTexture(_renderCloudsKernel, Shader.PropertyToID("_VolumeTexture"), _volume);
-                
+
                 cmd.SetComputeTextureParam(
                     _cloudsShader,
                     _renderCloudsKernel,
                     _cloudsLightingTextureRW_ID,
                     _CloudsLightingTextureRW.id
                 );
+                var invViewProjection = renderingData.cameraData.GetProjectionMatrix().inverse;
+                invViewProjection = renderingData.cameraData.GetViewMatrix().inverse * invViewProjection;
+                // invViewProjection = renderingData.cameraData.camera.cameraToWorldMatrix * invViewProjection;
+                cmd.SetComputeMatrixParam(
+                    _cloudsShader,
+                    Shader.PropertyToID("_CloudsPixelCoordToViewDirWS"),
+                    invViewProjection
+                );
                 cmd.SetComputeVectorParam(_cloudsShader, _traceScreenSice_ID, _TraceScreenSize);
                 cmd.DispatchCompute(_cloudsShader, _renderCloudsKernel, traceTX, traceTY, 1);
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
-                
-                cmd.Blit(_CloudsLightingTextureRW.id, renderingData.cameraData.renderer.cameraColorTarget);
+
+                cmd.Blit(
+                    new RenderTargetIdentifier(_CloudsLightingTextureRW.id), 
+                    renderingData.cameraData.renderer.cameraColorTarget, 
+                    _blitMat
+                );
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
             }
@@ -87,12 +104,22 @@ public class ComputeCloudsFeature : ScriptableRendererFeature
     public Texture cloudsVolume;
     CustomRenderPass m_ScriptablePass;
 
+    [SerializeField] private Shader _blitAdd = null;
+    [SerializeField] private Material _blitMat = null;
+
+
     /// <inheritdoc/>
     public override void Create()
     {
+#if UNITY_EDITOR
+        ResourceReloader.ReloadAllNullIn(this, UniversalRenderPipelineAsset.packagePath);
+        if (_blitAdd == null)
+            _blitAdd = Shader.Find("Hidden/LowKick/BlitAdd");
+        _blitMat = CoreUtils.CreateEngineMaterial(_blitAdd);
+#endif
         cloudsShader = Resources.Load<ComputeShader>("Clouds");
         cloudsVolume = Resources.Load<Texture>("VolumeCloud");
-        m_ScriptablePass = new CustomRenderPass(cloudsShader, cloudsVolume);
+        m_ScriptablePass = new CustomRenderPass(cloudsShader, cloudsVolume, _blitMat);
 
         // Configures where the render pass should be injected.
         m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingSkybox;
@@ -105,5 +132,3 @@ public class ComputeCloudsFeature : ScriptableRendererFeature
         renderer.EnqueuePass(m_ScriptablePass);
     }
 }
-
-
